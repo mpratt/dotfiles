739c2c23541f330ffe6ad3735bfe5b88990bb6a2
Michael Pratt <pratt@hablarmierda.net>
2014-09-04 10:28:01 -0500

    Added new functions into functions.sh - sinonimo() searches for sinonyms of a word - copyIt() is used on installation. It copies a file/dir.

--- bin/functions.d/functions.sh
+++ bin/functions.d/functions.sh
@@ -22,6 +22,36 @@ function symlinkIt()
     fi
 }
 
+# Copies a directory/file - Used in Installation files
+function copyIt()
+{
+    local src=$(realpath "${1}")
+    local dst=${2}
+
+    if [ -e "${src}" ]; then
+        echo "Copying $(basename ${src})"
+        if [ -f "${src}" ]; then
+            local dstsum=$(md5sum "${dst}" | cut -d ' ' -f1)
+            local srcsum=$(md5sum "${src}" | cut -d ' ' -f1)
+
+            if [[ "${dstsum}" == "${srcsum}" ]]; then
+                rm -rf ${dst}
+            else
+                read -p "Do you want to remove ${dst} before copying? (y/n) " RM
+                if [[ "${RM}" == "y" ]]; then
+                    rm -rf ${dst}
+                fi
+            fi
+            cp -r ${src} ${dst}
+        elif [ -d "${src}" ]; then
+            rsync --exclude .git -artvq ${src}/ ${dst}/
+        fi
+
+    else
+        echo "Could not find ${src}"
+    fi
+}
+
 # Go to a directory or file's parent. Move/Copy and follow
 function goto() { [[ -d "$1" ]] && cd "$1" || cd "$(dirname $1)"; }
 function cpf() { cp "$@" && goto "$_"; }
@@ -55,7 +85,8 @@ function date2stamp() { [ -z "$*" ] && echo "date2stamp Y-M-D h:m:s [2009-02-03 
 function gvim() { command gvim --remote-tab-silent $@ || command gvim $@; }
 
 # Search for sinonyms
-function sinonimo() {
+function sinonimo()
+{
     local baseUrl="http://www.wordreference.com/sinonimos"
     local word=$(echo ${1} | tr ' ' '+')
     exo-open "${baseUrl}/${word}"
