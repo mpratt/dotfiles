9624443661bf9716ea705b21dd4fb32d18a2980d
Michael Pratt <pratt@hablarmierda.net>
2014-09-04 10:33:33 -0500

    Initialize Thunar as a daemon when starting openbox

--- settings/openbox/autostart.sh
+++ settings/openbox/autostart.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # This shell script is run before Openbox launches.
+CWD=$(dirname $0)
 
 # Allow transparencies for tint2
 if which xcompmgr >/dev/null 2>&1; then
-    sleep 2
+    sleep 1
     xcompmgr &
 fi
+
+# Thunar Daemon
+thunar --daemon &
 
 # Change your keymap:
 setxkbmap -layout "us" -variant "intl"
 
 # Map the caps-lock key to Escape button
+#setxkbmap -option caps:escape
 xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 
 # Setup the wallpaper
 wallpaper --use-previous --use-random &
 
-
 # Start some apps
+tint2 -c "${CWD}/tint2rc" &
 nm-applet &
 xfce4-power-manager &
 volti &
-tint2 -c /home/pratt/.config/openbox/tint2rc
+[ -e "${CWD}/tomighty-0.7.1.jar" ] && java -jar "${CWD}/tomighty-0.7.1.jar" &
