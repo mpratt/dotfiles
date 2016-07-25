bb0a54f5068c6456710ba873aed2310161a215fa
Michael Pratt <pratt@hablarmierda.net>
2016-07-25 15:13:20 -0500

    Added new Template Files

---
+++ templates/PHP-File.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PHP-File.php
+ *
+ * @package
+ * @author Michael Pratt <pratt@hablarmierda.net>
+ * @link   http://www.michael-pratt.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace NS;
+
+/**
+ * The Main Body of this
+ */
+
