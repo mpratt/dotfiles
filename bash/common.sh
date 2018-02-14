748585d4a780a3b6dbd850a6f65ca974628369bc
Michael Pratt <commits@michael-pratt.com>
2018-02-14 12:47:42 -0500

    corrected composer path

--- bash/common.sh
+++ bash/common.sh
@@ -4,7 +4,7 @@
 # Append custom PATH
 [ -e "${HOME}/.bin" ] && PATH=${PATH}:${HOME}/.bin
 [ -e "${HOME}/.dotfiles/bin" ] && PATH=${PATH}:${HOME}/.dotfiles/bin
-[ -e "${HOME}/.composer/vendor/bin" ] && PATH=${PATH}:${HOME}/.composer/vendor/bin
+[ -e "${HOME}/.config/composer/vendor/bin" ] && PATH=${PATH}:${HOME}/.config/composer/vendor/bin
 
 # Allow root to use DISPLAY
 xhost + &> /dev/null
