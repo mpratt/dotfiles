ee593ca686b3b0a01c58b9d26fdb66575956a22a
Michael Pratt <commits@michael-pratt.com>
2018-02-14 12:44:07 -0500

    Added .composer/vendor/bin into $PATH

--- bash/common.sh
+++ bash/common.sh
@@ -4,6 +4,7 @@
 # Append custom PATH
 [ -e "${HOME}/.bin" ] && PATH=${PATH}:${HOME}/.bin
 [ -e "${HOME}/.dotfiles/bin" ] && PATH=${PATH}:${HOME}/.dotfiles/bin
+[ -e "${HOME}/.composer/vendor/bin" ] && PATH=${PATH}:${HOME}/.composer/vendor/bin
 
 # Allow root to use DISPLAY
 xhost + &> /dev/null
