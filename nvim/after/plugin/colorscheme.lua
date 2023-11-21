aea7e4794f649e6cf8f835d05a3f80a749971d61
Michael Pratt <commits@michael-pratt.com>
2023-11-21 17:00:54 -0500

    feat: updated nvim colorscheme

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,4 +1,4 @@
 vim.g.gruvbox_contrast_dark = 'hard'
 vim.g.gruvbox_invert_selection = '0'
 -- vim.cmd("colorscheme gruvbox")
-vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme rose-pine-moon')
