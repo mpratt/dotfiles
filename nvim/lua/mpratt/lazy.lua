69606710fa7bd6afe76b22933ef9bd59e7082f0a
Michael Pratt <commits@michael-pratt.com>
2024-07-24 22:46:12 -0500

    fix(nvim): Lazy should check for updates every 24 hours

--- nvim/lua/mpratt/lazy.lua
+++ nvim/lua/mpratt/lazy.lua
@@ -13,12 +13,8 @@ vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
     spec = {
-        -- import your plugins
         { import = "plugins" },
     },
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
-    -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = true, frequency = 86400 },
 })
