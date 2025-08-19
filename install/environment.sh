3efe8d0bf4d07dac30e42df01996cc7659394567
Michael Pratt <commits@michael-pratt.com>
2025-08-19 16:18:53 -0500

    chore: when installing env packages, do not reinstall already available packages

--- install/environment.sh
+++ install/environment.sh
@@ -2,7 +2,6 @@
 ##################################################################
 #  This scripts bootstraps all environment setup
 ##################################################################
-set -e
 
 if [[ "$(whoami)" == "root" ]]; then
     echo "Running as root is not recommended"
@@ -22,6 +21,6 @@ yay -S archlinux-keyring
 LIST=$(grep -v '^$\|^\s*\#' "${HOME}/.dotfiles/env/arch_packages.list")
 for pkg in ${LIST}; do
     echo "Installing: ${pkg}"
-    yay -S  --noconfirm ${pkg}
+    yay -S  --noconfirm --needed ${pkg}
     echo ""
 done
